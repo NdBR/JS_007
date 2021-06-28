47cbd03539c3f5e82c3ca576c1d6e32f1bb357b0
NdBR <84131697+NdBR@users.noreply.github.com>
2021-06-28 15:28:05 +0200

    Add files via upload

---
+++ JS_007/index.js
@@ -0,0 +1,18 @@
+// const painting = function (item) {
+//     console.log("painting the wall " + item);
+
+//     console.log("The wall has been painted " + item);
+// };
+
+// const painting_color = "red";
+// painting(painting_color);
+// painting();
+
+const painting = function (painting_area, painting_color) {
+
+    console.log("The " + painting_area + " has been painted " + painting_color);
+};
+const painting_color = "green";
+const painting_area = "north-wall";
+
+painting(painting_area, painting_color);
